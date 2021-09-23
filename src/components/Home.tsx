2516e2a53d40ef118831fc7a2763e677450b81f1
haglea <haglea@icloud.com>
2021-09-23 19:29:38 +0200

    Initial commit, typescript

---
+++ src/components/Home.tsx
@@ -0,0 +1,47 @@
+import { Carousel, Col, Container, Row } from 'react-bootstrap'
+import dishes from '../data/menu.json'
+import { useState } from 'react'
+import DishComments from './DishComments'
+import upperName from '../helpers/lib'
+import { RouteComponentProps } from 'react-router-dom'
+import { Dish } from '../types/Dish'
+
+interface HomeProps {
+  title: string
+}
+
+type MixedHomeProps = RouteComponentProps & HomeProps
+
+const Home = ({ title }: MixedHomeProps) => {
+  const [selected, setSelected] = useState<Dish | null>(null)
+
+  return (
+    <Container>
+      <Row className="justify-content-center mt-3">
+        <Col xs={12} md={6}>
+          <h1>Welcome to {upperName(title)}!</h1>
+          <h3 className="text-center mb-4">We can only cook pasta...</h3>
+          <Carousel>
+            {dishes.map((dish: Dish, i) => (
+              <Carousel.Item
+                key={dish.id}
+                onClick={() => {
+                  setSelected(dish)
+                }}
+              >
+                <img className="d-block w-100" src={dish.image} alt={'slide number ' + (i + 1)} />
+                <Carousel.Caption>
+                  <h3>{dish.name}</h3>
+                  <p>{dish.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Col>
+        <DishComments selectedPasta={selected} />
+      </Row>
+    </Container>
+  )
+}
+
+export default Home
---
+++ src/types/Reservation.ts
@@ -0,0 +1,9 @@
+export interface Reservation {
+        _id?: number
+        name: string
+        phone: string
+        numberOfPersons: number
+        smoking: boolean
+        dateTime: string
+        specialRequests: string
+}
